c7ff01a57d7484e64aa489b80ad9b070e200cad3
owenread <oread01p@gmail.com>
2026-03-14 14:43:11 -0600

    FIxed a few bugs and cleaned up UI

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,15 +2,30 @@ const menuButton = document.querySelector("#menu");
 const nav = document.querySelector("nav");
 
 menuButton.addEventListener("click", () => {
+
     nav.classList.toggle("hide");
+
+    menuButton.textContent = nav.classList.contains("hide") ? "☰" : "✖";
+
 });
 
 
-// footer year
+
+window.addEventListener("resize", () => {
+
+    if (window.innerWidth >= 700) {
+        nav.classList.remove("hide");
+        menuButton.textContent = "☰";
+    }
+
+});
+
+
+// Footer year
 const year = document.querySelector("#year");
 year.textContent = new Date().getFullYear();
 
 
-// last modified
+// Last modified
 const lastModified = document.querySelector("#lastModified");
 lastModified.textContent = "Last Modified: " + document.lastModified;
