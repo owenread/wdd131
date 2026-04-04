986cef8b661f30121c0bd4cb2341d9bb950c7ee6
owenread <oread01p@gmail.com>
2026-04-04 01:06:12 -0600

    Small Adjustments

--- scripts/form.js
+++ scripts/form.js
@@ -1,9 +1,9 @@
 const products = [
-    { id: "fc-1888", name: "flux capacitor", averagerating: 4.5 },
-    { id: "fc-2050", name: "power laces", averagerating: 4.7 },
-    { id: "fs-1987", name: "time circuits", averagerating: 3.5 },
-    { id: "ac-2000", name: "low voltage reactor", averagerating: 3.9 },
-    { id: "jj-1969", name: "warp equalizer", averagerating: 5.0 }
+    { id: "fc-1888", name: "Artemis II", averagerating: 4.5 },
+    { id: "fc-2050", name: "Kirby Sneakers", averagerating: 4.7 },
+    { id: "fs-1987", name: "Mjolnir", averagerating: 3.5 },
+    { id: "ac-2000", name: "Arc Reactor", averagerating: 3.9 },
+    { id: "jj-1969", name: "Mouskatool", averagerating: 5.0 }
 ];
 
 
